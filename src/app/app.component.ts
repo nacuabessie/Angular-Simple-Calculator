71e9f729123905b4713a317f930e118b71d91932
Bessie Jane Nacua <bessie.nacua@usjr.edu.ph>
2021-10-02 17:11:19 +0800

    added Angular Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SimpleCalculator';
+
+  num1='';
+  num2='';
+  result=0;
+  firstN=0;
+  secondN=0;
+
+  getFirstVal(value:string){
+    const num1 = parseInt(value);
+
+    if (!isNaN(num1)) {
+      this.firstN = num1;
+      console.log(this.firstN)
+    }
+  }
+  getSecondVal(value:string){
+    const num2 = parseInt(value);
+    if (!isNaN(num2)) {
+      this.secondN = num2;
+      console.log(this.secondN)
+    }
+  }
+
+  addition(){
+    this.result = this.firstN+this.secondN;
+  }
+
+  subtraction(){
+    this.result = this.firstN-this.secondN;
+  }
+
+  multiplication(){
+    this.result = this.firstN*this.secondN;
+  }
+  division(){
+    this.result = this.firstN/this.secondN;
+  }
 }
